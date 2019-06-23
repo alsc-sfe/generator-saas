fd2074c80331f94b26f27b8a89abb8798bd36a66
孤豹 <yongbao.fyb@koubei.com>
2019-06-23 19:53:57 +0800

    fix: edit saas config

--- generators/app/templates/pc/saas.config.js
+++ generators/app/templates/pc/saas.config.js
@@ -1,4 +1,6 @@
 module.exports = {
+  // saas应用类型
+  sat: 'pc',
   // 页面配置
   page: {
     index: {
@@ -9,19 +11,28 @@ module.exports = {
       bodys: [],
     },
   },
-  // 插件配置
-  plugins: [{
-    kbtracker: {
-    },
-    fireworm: false,
-    viewport: false,
-  }],
-  // 主题配置
-  themes: {},
-  // 全局head
-  heads: [],
-  // 全局body
-  bodys: [],
-  // 自定义构建配置、config为全局的构建配置
-  'builder': (config) => {}
+  // runtime相关配置
+  runtime: {
+    // 主题配置
+    themes: {},
+    // 全局head
+    heads: [],
+    // 全局body
+    bodys: [],
+    // 插件配置
+    plugins: [{
+      kbtracker: {
+      },
+      fireworm: false,
+      viewport: false,
+    }],
+  },
+  webpack: {
+    // 是否开启chunk分析
+    analyze: false,
+    // 自定义构建配置、config为全局的构建配置
+    config: (webpackConfig) => {
+      return webpackConfig;
+    }
+  }
 };
