52052e86ae847b72f3120be98f33d83437a16aea
zetay.lim <zetay.lim@alibaba-inc.com>
2020-03-13 20:14:44 +0800

    fix: add writing file

---
+++ generators/app/writing/saas-weapp-host.js
@@ -0,0 +1,43 @@
+const path = require('path');
+
+module.exports = function () {
+  const solutionPath = path.join(__dirname, '../templates/saas-weapp-host');
+  const distFolder = this.props.name;
+
+  this.fs.copy(
+    this.templatePath(`${solutionPath}/src/**`),
+    this.destinationPath(path.join(distFolder, 'src'))
+  );
+
+  [
+    'abc.json',
+    '.eslintignore',
+    '.eslintrc',
+    '.stylelintignore',
+    '.stylelintrc',
+    'commitlint.config.ts',
+    'app.config.ts',
+    'README.md',
+    'tsconfig.json',
+    'Jenkinsfile'
+  ].forEach(filename => {
+    this.fs.copy(
+      this.templatePath(`${solutionPath}/${filename}`),
+      this.destinationPath(path.join(distFolder, filename))
+    );
+  });
+
+  // 单独处理_gitignore
+  this.fs.copy(
+    this.templatePath(`${solutionPath}/_gitignore`),
+    this.destinationPath(path.join(distFolder, '.gitignore'))
+  );
+
+  ['package.json'].forEach(filename => {
+    this.fs.copyTpl(
+      this.templatePath(`${solutionPath}/${filename}`),
+      this.destinationPath(path.join(distFolder, filename)),
+      this.props
+    );
+  });
+}
