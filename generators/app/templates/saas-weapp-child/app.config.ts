0e426790af196f5e2ae1176c6dfdd000d0569472
zetay.lim <zetay.lim@alibaba-inc.com>
2020-03-14 01:48:30 +0800

    fix: 可指定父应用

--- generators/app/templates/saas-weapp-child/app.config.ts
+++ generators/app/templates/saas-weapp-child/app.config.ts
@@ -1,6 +1,9 @@
 module.exports = {
   appType: 'weAppChild',
   microAppName: 'myWeApp',
+  // 父应用URL，本地父子应用联调使用
+  // 默认使用内置父应用
+  // weAppHostUrl: [],
   // 路由前缀默认使用 /${microAppName}
   // 需要自定义则设置basename
   // basename: '/',
