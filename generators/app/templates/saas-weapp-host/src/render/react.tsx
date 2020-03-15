646bbd5427c40050edc596c543161cf839300151
zetay.lim <zetay.lim@alibaba-inc.com>
2020-03-15 22:38:36 +0800

    fix: 组件渲染去除layout

--- generators/app/templates/saas-weapp-host/src/render/react.tsx
+++ generators/app/templates/saas-weapp-host/src/render/react.tsx
@@ -1,39 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { Render } from '@saasfe/we-app/lib/weapp/base';
-import { Router, Route } from '@saasfe/we-app-react-router';
-import ChoiceLayout from '@choicefe/pc-we-app-layout';
 
 const reactRender: Render = {
-  mount(Component: any, container: Element, customProps = {}) {
-    const { pageScope = {}, basename } = customProps;
-    const { appName } = pageScope;
-    // @ts-ignore
-    const { route, routeIgnore, layout } = pageScope.getConfig();
-    const routerType = pageScope.getRouterType();
-
+  mount(Component: any, container: Element|null, customProps = {}) {
     ReactDOM.render(
-      (
-        <Router
-          microAppName={appName as string}
-          routerType={routerType}
-          basename={basename as string}
-        >
-          <Route
-            route={route}
-            routeIgnore={routeIgnore}
-            microAppName={appName}
-          >
-            <ChoiceLayout {...layout} emptyLayout={!layout}>
-              <Component {...customProps} />
-            </ChoiceLayout>
-          </Route>
-        </Router>
-      ),
-      container as Element
+      <Component {...customProps} />,
+      container as Element,
     );
   },
-  unmount(container?: Element) {
+  unmount(container: Element|null) {
     ReactDOM.unmountComponentAtNode(container as Element);
   },
 };
