0744b7463f4dd9ef0761d8098208ba9cb0d37bd4
zetay.lim <zetay.lim@alibaba-inc.com>
2020-03-16 09:09:48 +0800

    fix: hostApp

--- generators/app/templates/saas-weapp-child/app.config.ts
+++ generators/app/templates/saas-weapp-child/app.config.ts
@@ -1,12 +1,17 @@
 module.exports = {
   appType: 'weAppChild',
   microAppName: 'myWeApp',
-  // 父应用URL，本地父子应用联调使用
-  // 默认使用内置父应用
-  // weAppHostUrl: [],
+
+  // 父应用名称或静态资源地址，本地父子应用联调使用
+  // 父应用名称与wap上保持一致
+  // hostApp: 'cdn-child-test',
+  // 父应用静态资源地址
+  // hostApp: ['//localhost:8000/index.js'],
+
   // 路由前缀默认使用 /${microAppName}
   // 需要自定义则设置basename
   // basename: '/',
+
   // 页面配置
   page: [{
     module: 'index',
